960f1a8fd0ba472d147d8c61ea4403fb1b323f79
Armando <armandlim44@hotmail.com>
2020-09-18 12:58:05 -0500

    Arreglar README

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
 
 # COM11103 - Listas Ligadas
-<<<<<<< HEAD
 Javier Orcazas 192100
 Armando Limón Bautista 188471
-Santiago Fernández del Castillo Sodi 189210
+Santiago Fernández del Castillo 189210
 Fernando Colin 189644
-=======
-Javier Orcazas
-Armando Limón Bautista
-Fernando Colín ---
->>>>>>> 31c3f164523ca021418f41e80580a340ac46f78e
 
--- listas_ligadas.c
+++ listas_ligadas.c
@@ -16,22 +16,27 @@ struct complejo {
 int main(void) {
 	int a, b;
 
-	printf("Input two integers\n");
+	printf("Input two integers: \n");
 	scanf("%d%d", &a, &b);
 
 	struct node *inicial;
 	inicial = malloc(sizeof *inicial);
-	inicial->dato = a;
-	inicial->siguiente = malloc(sizeof *inicial);
-	inicial->siguiente->dato = b;
-	printf("primer dato %d\n", inicial->dato);
-	printf("segundo dato %d\n", inicial->siguiente->dato);
+	inicial -> dato = a;
+	inicial -> siguiente = malloc(sizeof *inicial);
+	inicial -> siguiente->dato = b;
+	printf("primer dato %d\n", inicial -> dato);
+	printf("segundo dato %d\n", inicial -> siguiente->dato);
 
 	struct complejo numero_complejo;
 	numero_complejo.real = 1;
 	numero_complejo.imaginario = -1;
 	printf("parte real  %f\n", numero_complejo.real);
 	printf("parte imaginario  %f\n", numero_complejo.imaginario);
+
+	struct complejo *ejemplo;
+	ejemplo = malloc(sizeof *ejemplo);
+	ejemplo -> real = 1.0;
+	ejemplo -> imaginario = 3.0;
 
 	return 0;
 }
